0f22eb89ecbf0b1559b6db09fd6657e416f68a8a
Kevin Ciardelli <ciardelk@bc.edu>
2023-08-23 15:02:10 -0700

    Adding Decoration to Intro Screen

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import '../styles/LandingPage.css'
+import GroupPic from '../assets/GroupPic.png'
+import LandingPicture from '../assets/LandingPicture.jpg'
 
 export const LandingPage = () => {
-    return (
-      <div className='Landing'>
-        <h1>Landing
-        </h1>
+  return (
+    <div>
+      <div className='BackgroundImage'></div>
+      <div className='LandingPageLayout'>
+
+        <div className='LandingText'>
+          Welcome To <br></br>Synergy Hip Hop Dance Company<br></br>Bio
+        </div>
+
+        <img src = {LandingPicture} className='Photo' ></img>
       </div>
-    );
-  };
+      <div className='SecondSection'>
+        Here
+      </div>
+    </div>
+  );
+};
