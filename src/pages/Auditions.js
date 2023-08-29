e6911f6c19f1a610415b8be69df80e7077975050
Kevin Ciardelli <ciardelk@bc.edu>
2023-08-28 23:34:37 -0400

    Auditions Page

--- src/pages/Auditions.js
+++ src/pages/Auditions.js
@@ -1,21 +1,26 @@
 import React from "react";
 import '../styles/Auditions.css'
+import AuditionImage from '../assets/GroupPic2.jpg'
 
 export const Auditions = () => {
     return (
       <div>
         <div className="AuditionFormat">
-        <div className='BackgroundImage'></div>
+        <div className='BackgroundImage2'></div>
 
-        <div className='LandingText'>
+        <div className='AuditionText'>
           Synergy's Auditions 2023-2024<br></br>Located at O'Connel House<br></br>September 3rd @ 11am
           <br></br>
           <br></br>
           No Dance Experience is Required, <br></br>Just Bring Yourself and Some Water!
           <br></br>
           We Will Be Providing Drinks and Snacks Throughout As Well!
+          <img src = {AuditionImage} className='AuditionsImage' ></img>
         </div>
         </div>
+        
+        
+        
       </div>
     );
   };
