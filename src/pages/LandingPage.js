30662887019c965f0177f00349ecdd0c339aef95
Kevin Ciardelli <ciardelk@bc.edu>
2023-08-31 13:58:01 -0400

    Adding content to the performances tab

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,7 +9,7 @@ export const LandingPage = () => {
       <div className='LandingPageLayout'>
 
         <div className='LandingText'>
-          Welcome To <br></br>Synergy Hip Hop Dance Company<br></br><br></br>Promoting Unity Among Students <br></br>of Diverse Cultural Backgrounds through<br></br> Hip Hop Dance <br></br> EST. 2004
+          Welcome To <br></br><a href='https://www.instagram.com/synstagrammm/'>Synergy Hip Hop Dance Company</a><br></br><br></br>Promoting Unity Among Students <br></br>of Diverse Cultural Backgrounds through<br></br> Hip Hop Dance <br></br> EST. 2004
         </div>
 
         <img src = {LandingPicture} className='Photo' ></img>
--- src/pages/Performances.js
+++ src/pages/Performances.js
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Video from '../assets/TestVideo.MOV'
+import '../styles/Performances.css'
 
 export const Performances = () => {
     return (
-      <div>
-        <div className="VideoBox">
-        <h1>Performances</h1>
-        
+        <div className="PerformancePage">
+            <div className="VideoBox">
+                <h1>Performances</h1>
+                <ln></ln>
+                <video controls className="Video">
+                    <source src={Video} type="video/quicktime" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
         </div>
-        
-      </div>
     );
-  };
+};
